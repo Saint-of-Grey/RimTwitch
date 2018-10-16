9f76324aeb431a1ec2fdc4c3defe87ec78dd3c5c
Alice Cecil <alycecil@gmail.com>
2018-10-16 17:26:05 -0400

    KA ad

--- Source/IRC/KeepAlive.cs
+++ Source/IRC/KeepAlive.cs
@@ -1,11 +1,13 @@
 using System.Threading;
+using Verse;
 
 namespace RimTwitch.IRC
 {
     public class KeepAlive : Loop
     {
         public KeepAlive(IrcClient irc) : base(irc)
-        {}
+        {
+        }
 
         // Send PING to irc server every 5 minutes
         protected override void Run()
@@ -13,9 +15,11 @@ namespace RimTwitch.IRC
             while (!Stopped)
             {
                 _irc.SendIrcMessage("PING irc.twitch.tv");
-                Thread.Sleep(300000); // 5 minutes
+                Thread.Sleep(100000);
+                if (Rand.Chance(.2f))
+                    _irc.SendPublicChatMessage(
+                        "RimWorld Twitch Bot (non-commercial) is Listening. Type !me to add yourself to the game. Support Girl Develop It - https://www.girldevelopit.com/donate");
             }
         }
-
     }
 }
